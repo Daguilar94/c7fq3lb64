8b08dbc40aa03077b9c423198e1e6e16ed031369
dguilar1994 <andrea@MacBook-Pro-de-Andrea.local>
2017-06-17 15:31:26 -0500

    Agregar solución al reto 11 de rails

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,9 @@
 class ExpensesController < ApplicationController
   def index
+    if (params[:concept] == nil) && (params[:category_id] == nil)
      @expenses = Expense.order("date DESC")
+   else
+     @expenses = Expense.where("concept like ? AND category_id like ?", "%#{params[:concept]}%", "%#{params[:category_id]}%").order("date DESC")
+   end
   end
 end
