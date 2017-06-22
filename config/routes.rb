c59799a2b146bc224623e704b8ef217d1e4b6aa8
dguilar1994 <andrea@MacBook-Pro-de-Andrea.local>
2017-06-22 02:13:03 -0500

    Agregar solucion conpleta

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  root 'expenses#index'
-  
-  resources :expenses, only: [:index]
+  devise_for :users
 
+  root 'expenses#index'
+  resources :users, except: [:new, :create, :index] do
+    resources :expenses, only: [:index]
+    member do
+      get 'expenses'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
